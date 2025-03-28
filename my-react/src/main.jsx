918d49c86ce2b61b03e786e3e1ec902d19d9653d
Jisoo Park <20231010@edu.hanbat.ac.kr>
2025-03-28 09:09:59 +0900

    REACT 수정 됨

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -1,11 +1,14 @@
-import {StrictMode} from 'react'
+import {StrictMode, useState} from 'react'
 import {createRoot} from "react-dom/client";
-import ForNest from "./ForNest.jsx";
-import books from "./books";
+import StyledPanel from "./ReactEx/StyledPanel.jsx";
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <ForNest src={books} />
+        <StyledPanel>
+            //children 프로퍼티
+            <p>회원 모집중!</p>
+            <p>위키북스 프로젝트에 오신것을 환영합니다!!</p>
+        </StyledPanel>
     </StrictMode>
 )
